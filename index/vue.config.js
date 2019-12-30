1dcca19d878918f143a18ec4370c5d09d3b18929
guoyu <2758813950@qq.com>
2019-12-30 15:57:18 +0800

    add:123

--- index/vue.config.js
+++ index/vue.config.js
@@ -15,7 +15,7 @@ module.exports={
 
       app.get('/api/detail',(req,res)=>{
         let {id}=req.query
-        let dataa=MovieData.MovieList.find(val=>val.id==id)
+        let dataa=MovieData.find(val=>val.id==id)
         res.json({
           adds:dataa,
           errCode:0
