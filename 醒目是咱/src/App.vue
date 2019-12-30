30f57be5d0701a0df8641b8dd5ba96db023036c5
guoyu <2758813950@qq.com>
2019-12-30 15:07:42 +0800

    移交xiangmu

---
+++ 醒目是咱/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <header class="header">
+      我是头部
+    </header>
+    <main class="main">
+      我是中间
+    </main>
+    <footer class="footer">
+      我是脚部
+    </footer>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+   
+  }
+}
+</script>
+
+<style lang='css'>
+*{
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+#app{
+  display: flex;
+  flex-direction: column;
+}
+.header{
+  width: 100%;
+  height: 50px;
+  background: orangered;
+}
+.main{
+  width: 100%;
+  flex: 1;
+  overflow: auto
+}
+.footer{
+  width: 100%;
+  height: 50px;
+  background: orangered;
+}
+</style>
